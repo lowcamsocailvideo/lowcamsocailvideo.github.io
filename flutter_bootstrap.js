aad405eff1279933bad154b9bb934b81d181a3a9
Lorne Cammack <lowcam.socailvideo@gmail.com>
2026-05-15 23:06:31 -0500

    Mobile browser updates pt 3.

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"42d3d75a56efe1a2e9902f52dc8006099c45d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "274273673" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2366048808" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
